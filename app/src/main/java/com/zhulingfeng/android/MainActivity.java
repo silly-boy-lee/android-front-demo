1e59ba4f42c166938b4306ca233078a61755992c
Administrator <2932582804@qq.com>
2017-03-01 09:08:15 +0800

    activity修改

--- app/src/main/java/com/zhulingfeng/android/MainActivity.java
+++ app/src/main/java/com/zhulingfeng/android/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String demo;
     }
 
 
