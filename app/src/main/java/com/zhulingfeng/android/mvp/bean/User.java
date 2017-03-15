68d7441e17924882c2c4df9c6672bf9eba421011
Mr.Lee <2932582804@qq.com>
2017-03-15 10:31:31 +0800

    更新

---
+++ app/src/main/java/com/zhulingfeng/android/mvp/bean/User.java
@@ -0,0 +1,26 @@
+package com.zhulingfeng.android.mvp.bean;
+
+/**
+ * Created by Administrator on 2017/3/15.
+ */
+
+public class User {
+    private String userName;
+    private String sex;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
